8c17138777fd3549c5a9c548a8f5c0fe51f01da7
Wallace Wallace <dbw5808@rit.edu>
2020-04-08 23:25:15 -0400

    networking error

--- client/app/maker.js
+++ client/app/maker.js
@@ -28,7 +28,7 @@ const DomoForm = (props) => {
         <input id="domoName" type="text" name="name" placeholder="Domo Name"/>
         <label htmlFor="age">Age: </label>
         <input id="domoAge" type="text" name="age" placeholder="DomoAge"/>
-            <input type="hidden" name="_csrf" value={props.csf}/>
+            <input type="hidden" name="_csrf" value={props.csrf}/>
             <input className="makeDomoSubmit" type="submit" value="Make Domo"/>
         </form>
     );
@@ -46,7 +46,7 @@ const DomoList = function(props) {
     const domoNodes = props.domos.map(function(domo){
         return(
         <div key={domo.id} className="domo">
-            <img src="/assets/img/domoface.jpg" alt="domo face" className="domoFace"/>
+            <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace"/>
                 <h3 className="domoName">Name: {domo.name}</h3>
                 <h3 className="domoAge">Age: {domo.age}</h3>
         </div>
