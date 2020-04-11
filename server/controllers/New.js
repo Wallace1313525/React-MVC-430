9794fa2bfeeb1084ac55bd809dd6177f40912024
Wallace Wallace <dbw5808@rit.edu>
2020-04-10 20:49:57 -0400

    emptied page

--- server/controllers/New.js
+++ server/controllers/New.js
@@ -2,37 +2,6 @@ const models = require('../models');
 
 const { New } = models;
 
-const makeDomo = (req, res) => {
-  if (!req.body.name || !req.body.age || !req.body.color) {
-    return res.status(400).json({ error: 'RAWR! Name, age, and color all required' });
-  }
-
-  const domoData = {
-    name: req.body.name,
-    age: req.body.age,
-    color: req.body.color,
-    owner: req.session.account._id,
-  };
-
-  const newDomo = new New.DomoModel(domoData);
-
-  const domoPromise = newDomo.save();
-
-  domoPromise.then(() => res.json({ redirect: '/maker' }));
-
-  domoPromise.catch((err) => {
-    // console.log(err);
-    if (err.code === 11000) {
-      return res.status(400).json({ error: 'Domo already exists' });
-    }
-
-    return res.status(400).json({ error: 'An error occured' });
-  });
-
-  return domoPromise;
-};
-
-
 const makerPage = (req, res) => {
   New.NewModel.findByOwner(req.session.account._id, (err, docs) => {
     if (err) {
@@ -43,21 +12,4 @@ const makerPage = (req, res) => {
   });
 };
 
-
-const getDomos = (request, response) => {
-  const req = request;
-  const res = response;
-
-  return New.NewModel.findByOwner(req.session.account._id, (err, docs) => {
-    if (err) {
-      //console.log(err);
-      return res.status(400).json({ error: 'An error occured' });
-    }
-
-    return res.json({ domos: docs });
-  });
-};
-
-module.exports.make = makeDomo;
-module.exports.getDomos = getDomos;
 module.exports.makerPage = makerPage;
