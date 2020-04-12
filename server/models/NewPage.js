34cbaa4b683e0e2b48787c1913dd3e33c61c9fbc
Wallace Wallace <dbw5808@rit.edu>
2020-04-11 20:51:03 -0400

    does it work

--- server/models/NewPage.js
+++ server/models/NewPage.js
@@ -1,58 +1,84 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const _ = require('underscore');
 
 let NewModel = {};
-
-const convertId = mongoose.Types.ObjectId;
-const setName = (name) => _.escape(name).trim();
-const setColor = (color) => _.escape(color).trim().toLowerCase();
+const iterations = 10000;
+const saltLength = 64;
+const keyLength = 64;
 
 const NewSchema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
     required: true,
     trim: true,
-    set: setName,
+    unique: true,
+    match: /^[A-Za-z0-9_\-.]{1,16}$/,
   },
-
-  age: {
-    type: Number,
-    min: 0,
+  salt: {
+    type: Buffer,
     required: true,
   },
-    
-  color: {
+  password: {
     type: String,
     required: true,
-    trim: true,
-    set: setColor,
   },
-
-  owner: {
-    type: mongoose.Schema.ObjectId,
-    required: true,
-    ref: 'Account',
-  },
-
-  createdData: {
+  createdDate: {
     type: Date,
     default: Date.now,
   },
 });
 
 NewSchema.statics.toAPI = (doc) => ({
-  name: doc.name,
-  age: doc.age,
+  // _id is built into your mongo document and is guaranteed to be unique
+  username: doc.username,
+  _id: doc._id,
 });
 
-NewSchema.statics.findByOwner = (ownerId, callback) => {
+const validatePassword = (doc, password, callback) => {
+  const pass = doc.password;
+
+  return crypto.pbkdf2(password, doc.salt, iterations, keyLength, 'RSA-SHA512', (err, hash) => {
+    if (hash.toString('hex') !== pass) {
+      return callback(false);
+    }
+    return callback(true);
+  });
+};
+
+NewSchema.statics.findByUsername = (name, callback) => {
   const search = {
-    owner: convertId(ownerId),
+    username: name,
   };
 
-  return NewModel.find(search).select('name age color').lean().exec(callback);
+  return NewModel.findOne(search, callback);
+};
+
+NewSchema.statics.generateHash = (password, callback) => {
+  const salt = crypto.randomBytes(saltLength);
+
+  crypto.pbkdf2(password, salt, iterations, keyLength, 'RSA-SHA512', (err, hash) => callback(salt, hash.toString('hex')));
+};
+
+NewSchema.statics.authenticate = (username, password, callback) => {
+  NewModel.findByUsername(username, (err, doc) => {
+    if (err) {
+      return callback(err);
+    }
+
+    if (!doc) {
+      return callback();
+    }
+
+    return validatePassword(doc, password, (result) => {
+      if (result === true) {
+        return callback(null, doc);
+      }
+
+      return callback();
+    });
+  });
 };
 
 NewModel = mongoose.model('New', NewSchema);
